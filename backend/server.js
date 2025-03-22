9be9df9d4052f2b993c038d9349b4b6a498fa169
Amit Verma <amitver6969@gmail.com>
2025-03-22 16:00:29 +0530

    db config

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const mongoose = require("mongoose");
-
-
+connectDB = require('./config/db');
+const rateLimit = require('express-rate-limit');
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+connectDB();
 // API Routes
 //app.use("/api/invoices", invoiceRoutes);
 
+
+const limit=rateLimit({
+  windowMs: 3 * 60 * 1000, // 15 minutes
+  max: 20, // Limit each IP to 100 requests per `window`
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+app.use(limit);
 app.get('/' , (req , res)=>{
-  res.send("namaste user !!!!");
+  res.json({message : "namaste - har har mahadev"});
 })
 
 const PORT = process.env.PORT || 5000;
